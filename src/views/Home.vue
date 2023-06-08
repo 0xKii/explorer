aeb9e8728bb12b51f977cfcf183de6db4e1812c6
sychonix <110755590+sychonix@users.noreply.github.com>
2023-06-08 23:04:36 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
         >
         <img src="logo.png" alt="logo" style="width:100px;height:100px;margin: 12px;" onContextMenu="return false;">
 
-          ALFONOVA<small class="flow-left"></small>
+          sychonix | EXPLORER<small class="flow-left"></small>
         </h1>
       </div>
     </b-link>
